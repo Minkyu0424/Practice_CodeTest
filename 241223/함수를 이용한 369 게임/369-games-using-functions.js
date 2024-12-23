6ff471bb0e1b7b96d87a055a39437cc2f60f5617
Miinkyuuu <yng1404@gmail.com>
2024-12-23 16:04:27 +0900

    Title: 함수를 이용한 369 게임, Time: 454ms, Memory: 34MB, Status: Passed - CodeTree

--- 241223/함수를 이용한 369 게임/369-games-using-functions.js
+++ 241223/함수를 이용한 369 게임/369-games-using-functions.js
@@ -1,4 +1,18 @@
 const fs = require("fs");
 const [a, b] = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
 
-console.log(b-a-2)
+let answer = 0;
+
+const isThree =(num) =>  num % 3 === 0
+
+const isTsn =(num)=>{
+     const digits = new Set(String(num));
+    return digits.has('3') || digits.has('6') || digits.has('9');
+}
+
+for(let i=a; i<=b; i++){
+    if(isThree(i) || isTsn(i)){
+        answer++
+    } 
+}
+    console.log(answer)
