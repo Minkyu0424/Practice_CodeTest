9226ad7a2aa7163f8ff62551393d29a871ec234d
Miinkyuuu <yng1404@gmail.com>
2024-12-24 15:18:47 +0900

    Title: 두 수의 거듭제곱, Time: 198ms, Memory: 15MB, Status: Passed - CodeTree

--- 241224/두 수의 거듭제곱/two-numbers-of-squares.js
+++ 241224/두 수의 거듭제곱/two-numbers-of-squares.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const [a, b] = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
 let sum = a
-for(let i=0; i<b; i++){
+for(let i=1; i<b; i++){
     sum=sum*a
 }
 console.log(sum)
