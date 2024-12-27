d27e56f4d53208dcab1be1a7cb58a142bea9e557
Miinkyuuu <yng1404@gmail.com>
2024-12-27 12:22:01 +0900

    Title: 사칙연산 함수, Time: 173ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241227/사칙연산 함수/quadratic-operations-function.js
+++ 241227/사칙연산 함수/quadratic-operations-function.js
@@ -1,8 +1,6 @@
 const fs = require(`fs`);
 const nums = fs.readFileSync('/dev/stdin').toString().split(' ');
-const calcs = '+-*/'
 
-if(calcs.includes(nums[1])){
     switch (nums[1]) {
     case '+':
         console.log(`${nums.join(' ')} = ${nums[0] + nums[2]}`);
@@ -17,9 +15,6 @@ if(calcs.includes(nums[1])){
         console.log(`${nums.join(' ')} = ${MAth.floor(nums[0] / nums[2])}`);
         break;
     default:
-        console.log('Invalid operation');
-        break;
-}
-}else{
         console.log('False')
+        break;
 }
