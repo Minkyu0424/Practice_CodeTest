dee9b7158fb1a68592fae7fff9fa7a9c7731b150
Miinkyuuu <yng1404@gmail.com>
2025-01-07 00:10:02 +0900

    Title: 배열 채우기, Time: 187ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250107/배열 채우기/filling-array.js
+++ 250107/배열 채우기/filling-array.js
@@ -6,5 +6,7 @@ const numArr = fs.readFileSync(0).toString().split(' ').map(Number).slice(0,10)
 for(i of numArr){
  i!== 0 && arr.push(i)
 }
-console.log(arr.reverse.join(' '))
+
+;
+console.log(arr.reverse().join(' '))
 
