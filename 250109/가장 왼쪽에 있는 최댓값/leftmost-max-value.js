238fcb9d289632210ab08de4a1fc9410a7e78310
Miinkyuuu <yng1404@gmail.com>
2025-01-09 21:08:31 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 176ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250109/가장 왼쪽에 있는 최댓값/leftmost-max-value.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const [cnt, numbers] = fs.readFileSync(0).toString().trim().split('\n')
+
+let point = cnt;
+let numberArr =  numbers.split(' ').map(Number);
+const answer=[]
+
+while(true){
+    const newArr = [...numberArr].sort((a, b) => b - a);
+    const maxNum = newArr[0]
+// console.log(maxNum,'최대치')
+
+    const maxWhere = numberArr.findIndex((num)=> num === maxNum);
+// console.log(maxWhere,'최대치 위치')
+    
+    if(maxWhere<=1){
+        answer.push(1);
+        break;
+    }
+    answer.push(maxWhere+1);
+    numberArr = numberArr.slice(0,maxWhere)
+}
+
+console.log(answer.join(' '))
