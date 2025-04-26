621af9904baed6d9c6118ac29df17464f77ce878
Miinkyuuu <yng1404@gmail.com>
2025-04-26 15:14:49 +0900

    [level 2] Title: 기능개발, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,16 @@
+function solution(progresses, speeds) {
+  const answer = [];
+  let timeArr = [];
+
+  progresses.forEach((work, i) => {
+    const releaseTime = Math.ceil((100 - work) / speeds[i]);
+    if (timeArr.length === 0 || timeArr[0] >= releaseTime) {
+      timeArr.push(releaseTime);
+    } else {
+      answer.push(timeArr.length);
+      timeArr = [releaseTime];
+    }
+  });
+  answer.push(timeArr.length);
+  return answer;
+}
