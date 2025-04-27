adffdf6de3cb9985f69bcf539d52c6a4233be72f
Miinkyuuu <yng1404@gmail.com>
2025-04-27 17:20:31 +0900

    내 코드로 변경

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -10,22 +10,21 @@ function solution(X, Y) {
     Y.split('').forEach((num) => {
         y[num] = (y[num] || 0) + 1;
     });
-
-    for (let i = 0; i < 10; i++) {
-        const strI = i.toString(); // 숫자 i를 문자열로 바꿔서 사용
-        if (x[strI] && y[strI]) {
-            let min = Math.min(x[strI], y[strI]);
-            for (let j = 0; j < min; j++) {
-                both.push(strI); // 문자열로 저장
-            }
-        }
-    }
-
-    if (both.length === 0) return "-1";
-    const result = both.sort((a, b) => b - a).join('');
     
-    // 만약 결과가 모두 '0'일 경우 '0'을 반환
-    if (result[0] === '0') return '0';
+    for(let i=0; i<10; i++){
+        const index = i.toString();
+        if(x[index] && y[index]){
+            let min = Math.min(x[index], y[index])
+            for(let j=0; j<min; j++){
+                both.push(index)
+            }
+        } 
+    }
+    if(both.length===0) return "-1"
 
-    return result;
+    const result = both.sort((a,b)=>b-a).join('')
+    if (result[0] === '0') return '0';
+    
+    return result
+    
 }
