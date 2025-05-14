441b2e321b1b9932ea338140ebd4d37f4d6b0f01
Miinkyuuu <yng1404@gmail.com>
2025-05-15 00:54:10 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 2.61 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -0,0 +1,28 @@
+function solution(video_len, pos, op_start, op_end, commands) {
+    const transNum = (str) => {
+        const time = str.split(':');
+        return parseInt(time[0]) * 60 + parseInt(time[1]);
+    };
+    
+    video_len = transNum(video_len);
+    pos = transNum(pos);
+    op_start = transNum(op_start);
+    op_end = transNum(op_end);
+
+    if (op_start <= pos && pos < op_end) pos = op_end;
+
+    for (let command of commands) {
+        if (command === 'prev') {
+            pos = Math.max(0, pos - 10);
+        } else if (command === 'next') {
+            pos = Math.min(video_len, pos + 10);
+        }
+
+        if (op_start <= pos && pos < op_end) pos = op_end;
+    }
+    console.log(pos)
+    const min = String(Math.floor(pos / 60)).padStart(2, '0');
+    const sec = String(pos % 60).padStart(2, '0');
+
+    return `${min}:${sec}`;
+}
